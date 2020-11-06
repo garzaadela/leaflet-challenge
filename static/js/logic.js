b87c67ea49376ba889cdb76a18eecddfcf0a5439
Adela Garza <a.garza045@gmail.com>
2020-11-05 18:03:18 -0600

    updated hw

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-// Define variables for our base layers
+// Define letiables for our base layers
 let streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     tileSize: 512,
@@ -28,9 +28,11 @@ function getColor(d) {
 }
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(data) {
+
     console.log(data)
     let earthquakes = L.geoJSON(data, {
         pointToLayer: function (feature, latlng) {
+
           console.log(latlng)
           let geojsonMarkerOptions = {
             radius: feature.properties.mag * 4,
@@ -41,6 +43,16 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geo
             fillOpacity: 0.7
           };
           return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup(
+            "Magnitude: "
+              + feature.properties.mag
+              + "<br>Depth: "
+              + feature.geometry.coordinates[2]
+              + "<br>Location: "
+              + feature.properties.place
+          );
         }
       }).addTo(myMap);
 // Define a baseMaps object to hold our base layers
@@ -55,8 +67,27 @@ let baseMaps = {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-    
-})
+
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function () {
+  
+      let div = L.DomUtil.create('div', 'info legend'),
+          grades = [0, 10, 30, 50, 70, 90],
+          labels = ['#ffffb2', '#fed976', '#feb24c', '#fd8d3c', '#f03b20', '#bd0026'];
+  
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (let i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + labels[i] + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+  
+      return div;
+  }
+  
+  legend.addTo(myMap); 
+});
 
 // set up the legend
 // legend = L.control({position: "bottomright"});
